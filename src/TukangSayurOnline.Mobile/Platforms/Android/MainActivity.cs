65d89d72cd1c0b6fa2c819589f42b9057628b7f0
Sonny Ariady <sonnyariady@gmail.com>
2026-08-07 16:54:30 +0700

    fix mobile issue

--- src/TukangSayurOnline.Mobile/Platforms/Android/MainActivity.cs
+++ src/TukangSayurOnline.Mobile/Platforms/Android/MainActivity.cs
@@ -1,4 +1,4 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
@@ -7,4 +7,9 @@ namespace TukangSayurOnline.Mobile;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        Android.Webkit.WebView.SetWebContentsDebuggingEnabled(true);
+    }
 }
